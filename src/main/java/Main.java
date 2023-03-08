a4b085fc46e0fdfc465fb6940426957bed660dd7
My PC <alex.petkov606@gmail.com>
2023-03-08 10:39:30 +0200

    asd

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,3 +1,4 @@
+import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
 
@@ -5,6 +6,10 @@ public class Main {
     public static void main(String[] args) {
 
         EntityManagerFactory emf = Persistence.createEntityManagerFactory("softuni");
+
+        EntityManager entityManager = emf.createEntityManager();
+
+
 
 
     }
